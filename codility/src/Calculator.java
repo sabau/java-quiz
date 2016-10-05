73cc7311bb947e6080b55ee6289edd2b4b2d1231
Karoly Albert Szabo <karoly.szabo@inetworking.it>
2016-10-05 17:59:11 +0200

    calculator implements

--- codility/src/Calculator.java
+++ codility/src/Calculator.java
@@ -1,8 +1,17 @@
-public class Calculator{
+class Calculator implements AdvancedArithmetic{
 	Calculator(){}
 
 	public int power(int n, int p) throws Exception {
 		if (n < 0 || p < 0) throw new Exception("n and p should be non-negative");
 		return (new Double(Math.pow(n,p))).intValue();
+	}
+
+	public int divisorSum(int n) {
+		if (n == 0) return 0;
+		int count = n;
+		for (int i = Math.round(n/2); i > 0; i--){
+			if (n % i == 0) count+=i;
+		}
+		return count;
 	}
 }
