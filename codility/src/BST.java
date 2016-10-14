956b03ee32c3e37c7aa8a0a0a4dccd550c7f837d
Karoly Albert Szabo <karoly.szabo@inetworking.it>
2016-10-14 14:50:24 +0200

    LinkedList

--- codility/src/BST.java
+++ codility/src/BST.java
@@ -1,14 +1,6 @@
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
-class Node{
-	Node left,right;
-	int data;
-	Node(int data){
-		this.data=data;
-		left=right=null;
-	}
-}
 
 public class BST {
 
---
+++ codility/src/Node.java
@@ -0,0 +1,10 @@
+
+class Node{
+	int data;
+	Node next, left,right;
+	Node(int d){
+		data=d;
+		left=right=next=null;
+	}
+
+}
